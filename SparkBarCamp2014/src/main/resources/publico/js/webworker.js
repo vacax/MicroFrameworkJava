812e6964a416f3afb65a9ef9e1e27799bd492cf4
vacax <carlosalfredocamacho@gmail.com>
2017-07-19 15:21:45 -0400

    Incluyendo funcionalidades de HTML5 .

---
+++ SparkBarCamp2014/src/main/resources/publico/js/webworker.js
@@ -0,0 +1,25 @@
+//haciendo la referencia al workers
+this.addEventListener('message', function(e) {
+
+    //la información la tenemos en la da.
+    var data = e.data;
+
+    //
+    switch (data.cmd) {
+        case 'inicio':
+            console.log("Presiono inicio..");
+            setInterval(function () {
+                this.postMessage('{"tipo": "fecha","mensaje" : "La fecha es: '+new Date().toUTCString()+'}');
+            }, 1000);
+            
+            this.postMessage('{"tipo": "msg","mensaje" : "WORKER INICIADO: '+data.msg+'}');
+
+            break;
+        case 'parada':
+            this.postMessage('{"tipo": "msg","mensaje" : "WORKER: '+data.msg+'}');
+            this.close(); // Termina el worker.
+            break;
+        default:
+            this.postMessage('{"tipo": "msg","mensaje" : "Mensaje no procesado: '+data.msg+'}');
+    };
+}, false);
